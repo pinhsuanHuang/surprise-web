7e83ae6618fb3e2e1139b9bab6f60af247e54cdf
Gary Huang <30866501+pinhsuanHuang@users.noreply.github.com>
2026-02-06 10:28:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const noBtn = document.querySelector('#noBtn');
 const yesBtn = document.querySelector('#yesBtn');
+const quizContainer = document.querySelector('#quizContainer');
+const resultContainer = document.querySelector('#resultContainer');
+const resultGif = document.querySelector('#resultGif');
+const resultText = document.querySelector('#resultText');
 
 function moveButton() {
     const windowWidth = window.innerWidth;
@@ -30,7 +34,23 @@ noBtn.addEventListener('touchstart', (e) => {
 });
 
 yesBtn.addEventListener('click', () => {
-    // 這裡可以換成你的 Google Form 邏輯或單純 Alert
-    alert('<3');
-    window.location.href = "https://www.google.com"; 
+    quizContainer.style.display = 'none';
+    resultContainer.style.display = 'block'; 
+    resultGif.src = "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExaDI5bm4yZ3B3eXcwZngwOTZ6YTg5cjMxZWswN3QxdHUzaGhpeWdoayZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3ofSBsGsk8TPcLaNws/giphy.gif"; 
+    resultText.textContent = 'I found a love for me.\nDarling, just dive right in and follow my lead\nWell, I found a girl, beautiful and sweet\nOh, I never knew you were the someone waiting for me';
+
+    setTimeout(() => {
+        window.location.href = "https://pinhsuanhuang.github.io/surprise-web"; 
+    }, 5000);
+});
+
+noBtn.addEventListener('click', () => {
+    quizContainer.style.display = 'none';
+    resultContainer.style.display = 'block'; 
+    resultGif.src = "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExamtkOWk5N2ZjanI1anhyaXRpcGx0ZWM0emMyamYyd3R1NHRqajZ5MCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/LTFmLb6e88cPz2sjux/giphy.gif"; 
+    resultText.textContent = 'You are not my lover, more like a brother!';
+
+    setTimeout(() => {
+        window.location.href = "https://pinhsuanhuang.github.io/surprise-web"; 
+    }, 5000);
 });
